b10f45901ffc0459170fe247e417b959508170bb
Fang <me@fangqk.com>
2022-11-15 00:30:32 +0800

    v0.12.0: upgrade dependencies

--- demo/apps/account-admin.ts
+++ demo/apps/account-admin.ts
@@ -3,16 +3,22 @@ import { TypicalSsoSdkPlugin } from '@fangcha/backend-kit/lib/sso'
 import { DemoConfig } from '../DemoConfig'
 import { AccountAdminPlugin } from '../../src/admin-sdk'
 import { MyAccountServer } from '../services/MyAccountServer'
+import { WebApp } from '@fangcha/backend-kit/lib/router'
 
-const app = new FangchaApp({
+const app = new WebApp({
   env: 'development',
   appName: 'account-admin',
+  routerOptions: {
+    backendPort: DemoConfig.adminPort,
+    baseURL: DemoConfig.adminBaseURL,
+    jwtProtocol: {
+      jwtKey: 'account_admin_jwt',
+      jwtSecret: DemoConfig.adminJwtSecret,
+    },
+  },
   plugins: [
     TypicalSsoSdkPlugin(DemoConfig.adminSSO),
     AccountAdminPlugin({
-      backendPort: DemoConfig.adminPort,
-      baseURL: DemoConfig.adminBaseURL,
-      jwtSecret: DemoConfig.adminJwtSecret,
       accountServer: MyAccountServer,
     }),
   ],
--- src/admin-sdk/AccountAdminPlugin.ts
+++ src/admin-sdk/AccountAdminPlugin.ts
@@ -3,33 +3,23 @@ import { AppPluginProtocol } from '@fangcha/backend-kit/lib/basic'
 import { KitProfileSpecDocItem } from '@fangcha/backend-kit/lib/profile'
 import { AccountServer } from '../AccountServer'
 import { AccountSpecDocItems } from '../admin-specs'
-import { RouterSdkPlugin } from '@fangcha/backend-kit/lib/router'
+import { _RouterState } from '@fangcha/backend-kit/lib/router'
 
 export interface AccountAdminOptions {
-  backendPort: number
-  baseURL: string
-  jwtSecret: string
-
   accountServer: AccountServer
 }
 
 export const AccountAdminPlugin = (options: AccountAdminOptions): AppPluginProtocol => {
-  const accountServer = options.accountServer
-  const routerApp = new RouterApp({
-    useHealthSpecs: true,
-    docItems: [KitProfileSpecDocItem, ...AccountSpecDocItems],
-  })
-  routerApp.addMiddlewareBeforeInit(async (ctx, next) => {
-    ctx.accountServer = accountServer
-    await next()
-  })
-  return RouterSdkPlugin({
-    baseURL: options.baseURL,
-    backendPort: options.backendPort,
-    routerApp: routerApp,
-    jwtProtocol: {
-      jwtKey: 'account_admin_jwt',
-      jwtSecret: options.jwtSecret,
+  return {
+    appWillLoad: () => {
+      const accountServer = options.accountServer
+      const routerApp = _RouterState.routerApp
+      routerApp.addDocItem(...AccountSpecDocItems)
+      routerApp.addMiddlewareBeforeInit(async (ctx, next) => {
+        ctx.accountServer = accountServer
+        await next()
+      })
     },
-  })
+    appDidLoad: () => {},
+  }
 }
